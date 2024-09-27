a745afb71704de0094bf27347853031580a61c12
preetrahul01 <preetikumarioct1998@gmail.com>
2024-09-27 15:16:40 +0530

    php

---
+++ signup.php
@@ -0,0 +1,10 @@
+<?php
+//print_r($_POST);
+if($_POST){
+    echo "user name is : ". $_POST['user_name'];
+    echo "<br/>";
+    echo "user name is : ". $_POST['user_password'];
+    echo "<br/>";
+    echo "user name is : ". $_POST['user_email'];
+}
+?>
